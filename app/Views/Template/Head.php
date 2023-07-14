fb859f0da34f33a92661e83981ff13f017a18632
AyrtonGuevara <guevaraaj@outlook.com>
2023-07-13 22:19:13 -0400

    apertura del branch develop

--- app/Views/Template/Head.php
+++ app/Views/Template/Head.php
@@ -3,13 +3,23 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- Latest compiled and minified CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Latest compiled JavaScript -->	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 	<title>
 		
 	</title>
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	<p>cabecera</p>
+	<nav class="navbar bg-dark">
+		<div class="container-fluid">
+			asd
+		</div>
+	</nav>	
 	<?php include('Menu.php') ?>
 	<?= $this -> renderSection('content') ?>
 	<?php include('Foot.php')?>
