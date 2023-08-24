8f0690d91729d235f2969415e8365b779657f698
AyrtonGuevara <guevaraaj@outlook.com>
2023-08-24 12:51:16 -0400

    eliminando el submodulo de Aulas y modificando el menu

--- app/Views/Template/Menu.php
+++ app/Views/Template/Menu.php
@@ -6,18 +6,24 @@
 <div id="sidenav" class="sidenav">
 	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 	<ul style="color:white;" id="accordion">
-		<li><a class="btn" data-bs-toggle="collapse" href="#col1">Usuario</a></li>
+		<li><a class="btn" data-bs-toggle="collapse" href="#col1">Cursos</a></li>
 		<ul id="col1" class="collapse" data-bs-parent="#accordion">
+			<li><a href="<?php echo base_url()?>cursos" id="item_collapse">Cursos</a></li>
+			<li><a href="<?php echo base_url()?>horarios" id="item_collapse">Horarios</a></li>
+		</ul>
+		<li><a class="btn" data-bs-toggle="collapse" href="#col2">Ambientes</a></li>
+		<ul id="col2" class="collapse" data-bs-parent="#accordion">
+			<li><a href="<?php echo base_url()?>ambientes" id="item_collapse">Ambientes</a></li>
+		</ul>
+		
+		<li>----------------------</li>
+		<li><a class="btn" data-bs-toggle="collapse" href="#col111">Usuario</a></li>
+		<ul id="col111" class="collapse" data-bs-parent="#accordion">
 			<li><a href="<?php echo base_url()?>usuario" id="item_collapse">Usuario</a></li>
 		</ul>
-		<li><a class="btn" data-bs-toggle="collapse" href="#col2">Personal</a></li>
-		<ul id="col2" class="collapse" data-bs-parent="#accordion">
+		<li><a class="btn" data-bs-toggle="collapse" href="#col112">Personal</a></li>
+		<ul id="col112" class="collapse" data-bs-parent="#accordion">
 			<li><a href="<?php echo base_url()?>personal" id="item_collapse">Personal</a></li>
-		</ul>
-		<li> <a class="btn" data-bs-toggle="collapse" href="#col3">Aulas</a></li>
-		<ul id="col3" class="collapse" data-bs-parent="#accordion">
-			<li><a href="<?php echo base_url()?>aula" id="item_collapse">Aulas</a></li>
-			<li><a href="<?php echo base_url()?>ubicacion" id="item_collapse">Ubicacion</a></li>
 		</ul>
 	</ul>
 </div>
